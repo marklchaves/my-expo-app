5a3a7064d43e1e8957f13a3799a5ae621e8e34d3
mark l chaves <mark@marklchaves.com>
2021-01-09 18:11:40 +0800

    Initial check-in
    First working version plus first refactoring iteration (styles.js).

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing'; 
+
+import { styles } from './styles.js';
+
+const Home = (props) => (
+  <View style={styles.container}>
+    <Text style={styles.heading}>Open up App.js to start working on your app!</Text>
+    <StatusBar style="auto" />
+
+    <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
+    <Text style={styles.instructions}>
+      To share a photo from your phone with a friend, just press the button below!
+    </Text>
+
+    <TouchableOpacity
+      onPress={props.openImagePickerAsync}
+      style={styles.button}>
+      <Text style={styles.buttonText}>Pick a photo</Text>
+    </TouchableOpacity>
+  </View>
+);
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log('pickerResult = ', pickerResult);
+
+    if (pickerResult.cancelled === true) {
+      return; 
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  let openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`Uh oh, sharing isn't available on your platform`);
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  }; 
+
+  // TO DO: Build cancel option here. Look at CS50M
+  //        toggling state.
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home openImagePickerAsync={openImagePickerAsync} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+    color: '#f5f5f5',
+    fontSize: 32,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 5,
+    marginBottom: 5,
+    padding: 20,
+    borderRadius: 5,
+    backgroundColor: 'blue',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#f5f5f5',
+    padding: 10,
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  }
+});
